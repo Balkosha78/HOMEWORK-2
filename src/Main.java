19d08ad4117e72cf5e05d24423d7a5f2b430eeec
Balkosha78 <balko-aleksey@yandex.ru>
2023-01-24 18:56:54 +0700

    Выполнены задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");//Код задачи 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");//Код задачи 2
+        var dog = 8.0;
+        dog = dog + 4;
+        var cat = 3.6;
+        cat = cat + 4;
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");//Код задачи 3
+        var dog = 8.0;
+        dog = dog - 3.5;
+        var cat = 3.6;
+        cat = cat - 1.6;
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
